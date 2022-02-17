11ce1ee7458390fdb2ba658e67c8bf72c8cf1ca2
UttamSaha <uttamsaha982@gmail.com>
2022-02-17 16:03:31 +0600

    validation added

--- js/income.js
+++ js/income.js
@@ -4,6 +4,34 @@ function getInputvalue(inputID){
     return inputFieldValue;
 }
 
+function getID(id){
+    const docID = document.getElementById(id);
+    return docID;
+}
+
+function displayError(id,numberError,negativeError){
+    const inputField = getID(id);
+    const display = getID(numberError);
+    const display2 = getID(negativeError);
+    const inputFieldValue = inputField.value;
+  
+    if(inputFieldValue == '-'){
+        display2.style.display = 'block';
+        display.style.display = 'none';
+        inputField.value = '';
+    }
+    else if(isNaN(inputFieldValue)){
+        display.style.display = 'block';
+         display2.style.display = 'none';
+        inputField.value = '';
+
+        // alert('Enter number value');
+    }
+    if(inputFieldValue >= 0){
+        display.style.display = 'none';
+        display2.style.display = 'none';
+    }
+}
 
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
@@ -12,11 +40,11 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const rentInputValue = getInputvalue('rent-input');
     const clothInputValue = getInputvalue('cloth-input');
 
-    const totalExpenses = document.getElementById('total-expenses');
+    const totalExpenses = getID('total-expenses');
     const updatedTotalExpenses = foodInputValue + rentInputValue + clothInputValue;
     totalExpenses.innerText = updatedTotalExpenses;
 
-    const balanceText = document.getElementById('balance-text');
+    const balanceText = getID('balance-text');
     balanceText.innerText = incomeInputValue - updatedTotalExpenses;
 });
 
@@ -24,13 +52,31 @@ document.getElementById('save-btn').addEventListener('click', function(){
     const incomeInputValue = getInputvalue('income-input');
     const savingInputValue = getInputvalue('saving-input');
     
-    const savingAmount = document.getElementById('saving-amount');
+    const savingAmount = getID('saving-amount');
     const updatedSavngAmount = (incomeInputValue * (savingInputValue/100));
     savingAmount.innerText = updatedSavngAmount;
 
     const balanceText = document.getElementById('balance-text').innerText;
-    const remainingBalace = document.getElementById('remaining-balance');
+    const remainingBalace = getID('remaining-balance');
     remainingBalace.innerText = parseFloat(balanceText) - updatedSavngAmount;
+})
 
+document.getElementById('income-input').addEventListener('keyup', function(){
+    displayError('income-input','number-error','negative-error');
+})
 
+document.getElementById('food-input').addEventListener('keyup', function(){
+    displayError('food-input','number-error2','negative-error2');
+})
+
+document.getElementById('rent-input').addEventListener('keyup', function(){
+    displayError('rent-input','number-error3','negative-error3');
+})
+
+document.getElementById('cloth-input').addEventListener('keyup', function(){
+    displayError('cloth-input','number-error4','negative-error4');
+})
+
+document.getElementById('saving-input').addEventListener('keyup', function(){
+    displayError('saving-input','number-error5','negative-error5');
 })
