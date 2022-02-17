dc440e367e0034334cd8284d1bd3fa5dc1c48cd6
UttamSaha <uttamsaha982@gmail.com>
2022-02-17 11:42:14 +0600

    shared function added

--- js/income.js
+++ js/income.js
@@ -1,27 +1,29 @@
+function getInputvalue(inputID){
+    const inputField = document.getElementById(inputID);
+    const inputFieldValue = parseFloat(inputField.value);
+    return inputFieldValue;
+}
+
+
+
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    const incomeInput = document.getElementById('income-input');
-    const incomeInputValue = parseFloat(incomeInput.value);
-    const foodInput = document.getElementById('food-input');
-    const foodInputValue = parseFloat(foodInput.value);
-    const rentInput = document.getElementById('rent-input');
-    const rentInputValue = parseFloat(rentInput.value);
-    const clothInput = document.getElementById('cloth-input');
-    const clothInputValue = parseFloat(clothInput.value);
+    const incomeInputValue = getInputvalue('income-input');
+    const foodInputValue = getInputvalue('food-input');
+    const rentInputValue = getInputvalue('rent-input');
+    const clothInputValue = getInputvalue('cloth-input');
 
     const totalExpenses = document.getElementById('total-expenses');
     const updatedTotalExpenses = foodInputValue + rentInputValue + clothInputValue;
     totalExpenses.innerText = updatedTotalExpenses;
 
     const balanceText = document.getElementById('balance-text');
-    // const balanceTextValue = balanceText.innerText;
     balanceText.innerText = incomeInputValue - updatedTotalExpenses;
 });
 
 document.getElementById('save-btn').addEventListener('click', function(){
-    const incomeInput = document.getElementById('income-input');
-    const incomeInputValue = parseFloat(incomeInput.value);    
-    const savingInput = document.getElementById('saving-input');
-    const savingInputValue = parseFloat(savingInput.value);
+    const incomeInputValue = getInputvalue('income-input');
+    const savingInputValue = getInputvalue('saving-input');
+    
     const savingAmount = document.getElementById('saving-amount');
     const updatedSavngAmount = (incomeInputValue * (savingInputValue/100));
     savingAmount.innerText = updatedSavngAmount;
